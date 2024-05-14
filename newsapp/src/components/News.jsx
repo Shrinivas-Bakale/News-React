a24631c67fb818ebcbcb33b71df93f92ea4e1e16
Shinu <shrinivasbakale2002@gmail.com>
2024-05-14 15:55:05 +0530

    Next and previous added

--- newsapp/src/components/News.jsx
+++ newsapp/src/components/News.jsx
@@ -1,31 +1,83 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
+import axios from "axios";
 
-export class News extends Component {
-  render() {
-    return (
-      <div className="container my-3">
-        <div className="row">
-          <div className="col-md-4">
-            <NewsItem
-              title="3 Atlanta police officers wounded in shooting, suspect dead - Fox News"
-              description="Three Atlanta police officers are expected to survive after being shot while responding to a call about an armed individual on Saturday evening."
-              image="https://static.foxnews.com/foxnews.com/content/uploads/2024/05/atlanta-pd-shooting2.jpg"
-            />
-          </div>
-          <div className="col-md-4">
-            <NewsItem />
-          </div>
-          <div className="col-md-4">
-            <NewsItem />
-          </div>
-          <div className="col-md-4">
-            <NewsItem />
-          </div>
-        </div>
+function News() {
+  const [articles, setArticles] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const getApi = async (pageNumber) => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f0f20faac89a4cb39ddbebe92a2f2336&page=${pageNumber}`;
+    try {
+      const response = await axios.get(url);
+      setArticles(response.data.articles);
+      setTotalPages(Math.ceil(response.data.totalResults / 20)); // Assuming 20 articles per page
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getApi(currentPage);
+  }, [currentPage]);
+
+  const handleNextBtn = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousBtn = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  return (
+    <div className="container my-3">
+      <h2>News articles</h2>
+      <div className="row">
+        {articles.map((element) => {
+          return (
+            <div className="col-md-4" key={element.url}>
+              <NewsItem
+                title={element.title ? element.title.slice(0, 45) : ""}
+                description={
+                  element.description ? element.description.slice(0, 88) : ""
+                }
+                image={
+                  element.urlToImage
+                    ? element.urlToImage
+                    : "https://www.hindustantimes.com/ht-img/img/2024/05/12/1600x900/phase_4_1715514229523_1715514243628.jpg"
+                }
+                newsUrl={element.url}
+                date={element.publishedAt}
+              />
+            </div>
+          );
+        })}
       </div>
-    );
-  }
+      <div className="container d-flex justify-content-between my-3">
+        <button
+          type="button"
+          className="btn btn-dark"
+          onClick={handlePreviousBtn}
+          disabled={currentPage <= 1}
+        >
+          &larr; Previous
+        </button>
+        <button
+          type="button"
+          className="btn btn-dark"
+          onClick={handleNextBtn}
+          disabled={currentPage >= totalPages}
+        >
+          Next &rarr;
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default News;
--- newsapp/src/components/NewsItem.jsx
+++ newsapp/src/components/NewsItem.jsx
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, image } = this.props;
+    let { title, description, image, newsUrl, date } = this.props;
     return (
       <div>
         <div className="card" style={{ width: "18rem" }}>
           <img src={image} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="fs-6">{date}</p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
+              Read More
             </a>
           </div>
         </div>
